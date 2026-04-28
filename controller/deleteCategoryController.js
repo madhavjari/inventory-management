ebe7f892716bf9c5fa42a73e4054a6efb312a46c
madhavjari <213913746+madhavjari@users.noreply.github.com>
2026-04-28 16:31:43 +0530

    deletion bug fix

--- controller/deleteCategoryController.js
+++ controller/deleteCategoryController.js
@@ -3,8 +3,9 @@ const db = require("../db/queries");
 async function deleteCategoryPost(req, res) {
   const categoryId = req.params.id;
   const categoryFound = await db.searchCategoryInProduct(categoryId);
-  console.log(categoryFound);
-  if (categoryFound) return res.redirect("/categoryList?error=in_use");
+  console.log(categoryFound.length);
+  if (categoryFound.length != 0)
+    return res.redirect("/categoryList?error=in_use");
   else await db.deleteCategory(categoryId);
   res.redirect("/categoryList");
 }
